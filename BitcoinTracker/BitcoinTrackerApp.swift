90694efd368e7bf62b07417a3ca94e046c292048
Giohji Hoshino <giohji@gmail.com>
2025-05-20 18:53:11 +0100

    feat: Initial project setup with SwiftUI, XCTest, and .gitignore

---
+++ BitcoinTracker/BitcoinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinTrackerApp.swift
+//  BitcoinTracker
+//
+//  Created by Guilherme Giohji Hoshino on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
